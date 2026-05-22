b5d31c1dd5cede8123afa274adc7a4119fb7940c
sean tietz <sean.tietz2@gmail.com>
2026-05-22 13:00:16 +0200

    fix: replace deprecated system with stdenv.hostPlatform.system

--- flake.nix
+++ flake.nix
@@ -105,7 +105,7 @@
 
             package = lib.mkOption {
               type = lib.types.package;
-              default = self.packages.${pkgs.system}.default;
+              default = self.packages.${pkgs.stdenv.hostPlatform.system}.default;
               description = "The netpala package to use";
             };
           };
@@ -120,7 +120,7 @@
 
       # Overlay for use with other flakes
       overlays.default = final: prev: {
-        netpala = self.packages.${prev.system}.default;
+        netpala = self.packages.${prev.stdenv.hostPlatform.system}.default;
       };
     };
 }
